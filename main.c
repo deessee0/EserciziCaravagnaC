98cf8597ec6c151df75c40e771f6baa0d2bf96c7
Alberto Di <alberto.disalvia@gmail.com>
2020-12-09 10:51:52 +0000

    maledetti puntatori

--- main.c
+++ main.c
@@ -1,13 +1,5 @@
 #include<stdio.h>
 
-void ordine_crescente(int* a, int* b, int* c, int* d)
-{
-    if(*a > *b)
-    {
-        
-    }
-}
-
 void scambia(int* a, int* b)
 {
     int tmp;
@@ -16,16 +8,57 @@ void scambia(int* a, int* b)
     *b = tmp;
 }
 
-int calcola_minimo(int* a, int* b)
+int min(int* a, int *b) 
 {
-    if(*a > *b) return *a;
-    else return *b;
+    int min;
+
+    if(*a<=*b)
+    {
+        min = *a;
+    }   else   {
+        min = *b;
+    }
+  
+    return min;
+}
+
+void ordine_crescente(int* a, int* b, int* c, int* d)
+{
+    int tmp;
+
+    tmp = min(a,b);
+    printf("\nla var tmp: %d\n", tmp);
+    scambia(&tmp, b);
+
+    tmp = min(b, c);
+    printf("\nla var tmp: %d\n", tmp);
+    scambia(&tmp, c);
+
+    tmp = min(c, d);
+    printf("\nla var tmp: %d\n", tmp);
+    scambia(&tmp, d);
+    
 }
 
 int main(void) 
 {
-    
+    int risultato, a, b, c, d;
 
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &a);
+
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
+
+    printf("Inserisci il terzo numero: ");
+    scanf("%d", &c);
+
+    printf("Inserisci il quarto numero: ");
+    scanf("%d", &d);
+
+    ordine_crescente(&a, &b, &c, &d);
+    printf("-----------------------------------");
+    printf("\nPrimo: %d\nSecondo: %d\nTerzo: %d\nQuarto: %d",a,b,c,d);
 
     return 0;
 
