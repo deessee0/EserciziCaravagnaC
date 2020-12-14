413a53d7decea11207b569db8da4f29559d3d961
Alberto Di <alberto.disalvia@gmail.com>
2020-12-14 14:33:31 +0000

    daghe

---
+++ 23ottobre/es3.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+
+void calcolo_ipotenusa(float *ac, float *gamma, float *bc)
+{
+   
+           
+}
+
+void calcolo_perimetro(float ac, float ab, float *bc)
+{
+   
+           
+}
+
+void calcolo_altezza()
+{
+
+}
+
+float calcolo_area(float *ab, float *h)
+{
+
+}
+
+int main(void) 
+{
+    float ac;
+    float gamma;
+    int alfa = 90;
+
+    printf("Inserisci il valore del cateto minore: ");
+    scanf("%f", &ac);
+
+    printf("Inserisci il valore dell'angolo gamma: ");
+    scanf("%f", &gamma);
+
+
+
+    return 0;
+
+}
--- main.c
+++ main.c
@@ -1,29 +1,56 @@
 #include<stdio.h>
+#include<math.h>
 
-void somma_armonica(int n, double* h)
+void calcolo_ipotenusa(float *ac, float *gamma, float *ipotenusa)
 {
-    double temp;
-
-    if(n == 1)
-    {
-        *h = 1.0 + *h;
-    } else {       
-        *h =  *h + (1.0/n);
-        somma_armonica(n-1,h);     
-    }
+    double angolo_gamma = *gamma;
+    *ipotenusa = *ac / sin(angolo_gamma);  
            
+}
+
+float calcolo_perimetro(float ac, float ipotenusa, float *bc)
+{
+   return ac + ipotenusa + *bc;
+}
+
+void calcolo_area(float *perimetro, float ac, float *ipotenusa, float *bc, float *area)
+{
+    double parziale = (*perimetro/2) * (*perimetro/2 - ac) * (*perimetro/2 - *ipotenusa) * (*perimetro/2 - *bc);
+
+    *area = sqrt(parziale);
+}
+
+float calcolo_altezza(float *ab, float *h)
+{
+    return ((*ab * *h) / 2);
 }
 
 int main(void) 
 {
-    int n;
-    double risultato;
+    float ac, gamma, bc=15;
+    int alfa = 90;
+    float ipotenusa, perimetro, altezza, area;
 
-    printf("Inserisci n: ");
-    scanf("%d", &n);
+    printf("\n-------------------------------------\n");
+    printf("Inserisci il valore del cateto minore: ");
+    scanf("%f", &ac);
 
-    somma_armonica(n, &risultato);
-    printf("Risultato = %lf", risultato);
+    printf("Inserisci il valore dell'angolo gamma: ");
+    scanf("%f", &gamma);
+    printf("\n-------------------------------------\n");
+
+    calcolo_ipotenusa(&ac, &gamma, &ipotenusa);
+    printf("L'ipotenusa del triangolo è: %f\n", ipotenusa);
+
+    perimetro = calcolo_perimetro(ac, ipotenusa, &bc);
+    printf("Il perimetro del triangolo è: %f\n", perimetro);
+
+    calcolo_area(&perimetro, ac, &ipotenusa, &bc, &area);
+    printf("L'area del triangolo è: %f\n", area);
+
+    altezza = calcolo_altezza(&ipotenusa, &area);
+    printf("L'altezza del triangolo è: %f\n", altezza);
+
 
 
     return 0;
